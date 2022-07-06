b439129bf60feb532c89faf9e9308da288db5d07
Gui Sato <satogui5@gmail.com>
2022-07-05 21:13:14 -0300

    Add view Login e mensagens de Erro

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -3,8 +3,12 @@ from django.http import HttpResponse
 from .utils import password_is_valid
 from django.shortcuts import redirect
 from django.contrib.auth.models import User
+from django.contrib.messages import constants
+from django.contrib import messages
+from django.contrib import auth
 
 # Create your views here.
+
 
 def cadastro(request):
     if request.method == 'GET':
@@ -14,20 +18,38 @@ def cadastro(request):
         email = request.POST.get('email')
         senha = request.POST.get('senha')
         confirmar_senha = request.POST.get('confirmar_senha')
-        
+
         if not password_is_valid(request, senha, confirmar_senha):
             return redirect('/auth/cadastro')
-        
+
         try:
             user = User.objects.create_user(username=usuario,
-                                            email = email,
                                             password=senha,
                                             is_active=False)
             user.save()
+            messages.add_message(request, constants.SUCESS,
+                                 'Usuario cadastrado com sucesso')
             return redirect('/auth/logar')
+        
         except:
+            messages.add_message(request, constants.ERROR,
+                                 'Erro interno do sistema')
             return redirect('/auth/cadastro')
-         
-    
+
+
 def logar(request):
-    return render(request, 'login.html')
+    if request.method == "GET":
+        return render(request, 'logar.html')
+    elif request.method == "POST":
+        username = request.POST.get('usuario')
+        senha = request.POST.get('senha')
+
+        usuario = auth.authenticate(username=username, password=senha)
+
+        if not usuario:
+            messages.add_message(request, constants.ERROR,
+                                 'Username ou senha inválidos')
+            return redirect('/auth/logar')
+        else:
+            auth.login(request, usuario)
+            return redirect('/')
